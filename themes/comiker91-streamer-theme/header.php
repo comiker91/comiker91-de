f560985736762eacb18154e1b6b3cd378fd89bdc
Bastian <66472554+comiker91@users.noreply.github.com>
2026-08-27 17:20:35 +0200

    theme: load imported enhancement styles

--- themes/comiker91-streamer-theme/header.php
+++ themes/comiker91-streamer-theme/header.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri() . '/assets/css/enhancements.css?ver=' . C91_VERSION); ?>">
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>

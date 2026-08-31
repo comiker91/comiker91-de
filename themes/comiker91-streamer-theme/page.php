12e0295263b5b2704568b732c0bc1ebf84b654d5
Bastian <66472554+comiker91@users.noreply.github.com>
2026-08-31 18:23:37 +0200

    Add breadcrumbs to static pages

--- themes/comiker91-streamer-theme/page.php
+++ themes/comiker91-streamer-theme/page.php
@@ -1,4 +1,5 @@
 <?php get_header(); while(have_posts()): the_post(); ?>
+<?php c91_breadcrumbs(); ?>
 <section class="page-hero"><div class="shell narrow"><span class="eyebrow">COMIKER91</span><h1><?php the_title(); ?></h1></div></section>
 <section class="section"><div class="shell narrow content-card prose"><?php the_content(); ?></div></section>
 <?php endwhile; get_footer(); ?>
